bf60b4a562a4d4112ffff159d7265a4d24fa7fc5
iwtn <ganta.viii@gmail.com>
2018-08-19 12:43:04 +0900

    view profiles from github

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,35 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
       <h1 class="title">
-        snscard
+        {{ login }}
       </h1>
-      <h2 class="subtitle">
-        your business card from sns accounts
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <img :src="avatar_url" />
+      <ul class="links">
+        <li>{{bio}}</li>
+        <li>{{blog}}</li>
+      </ul>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+const octokit = require('@octokit/rest')()
+
 
 export default {
-  components: {
-    AppLogo
+  asyncData () {
+    return octokit.users.getForUser({
+      username: 'iwtn'
+    }).then(({data, headers, status}) => {
+      console.log(data)
+      return {
+        login: data.login,
+        avatar_url: data.avatar_url,
+        bio: data.bio,
+        blog: data.blog
+      }
+    })
   }
 }
 </script>
@@ -45,21 +47,15 @@ export default {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 80px;
   color: #35495e;
   letter-spacing: 1px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
 .links {
-  padding-top: 15px;
+  text-align: left;
+  font-size: 20px;
+  list-style-type: none;
 }
 </style>
 
