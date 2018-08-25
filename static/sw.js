a33c75b15ed018f6345e7a0e5983544953ab3a4b
iwtn <ganta.viii@gmail.com>
2018-08-25 23:19:23 +0900

    push dist

--- static/sw.js
+++ static/sw.js
@@ -8,24 +8,24 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/snscard/_nuxt/app.b843ace77a92b4f759cb.js",
-    "revision": "8fae7e9c43224abcd3e59ef1ae1dd743"
+    "url": "/snscard/_nuxt/app.840429a318521f2d7cda.js",
+    "revision": "511eba2fb3aa629baa5f169ee5640fdb"
   },
   {
     "url": "/snscard/_nuxt/layouts/default.3c200211a08bad610e83.js",
     "revision": "59b0a1cafbce7b40f320566fcf2283cd"
   },
   {
-    "url": "/snscard/_nuxt/manifest.df6793fe8e3fee8b4958.js",
-    "revision": "e63e72bbcdba0ebf1889cce2e580ebed"
+    "url": "/snscard/_nuxt/manifest.1ab68d87361dad445719.js",
+    "revision": "4ff27e626207b4eacbec3c5dec980c79"
   },
   {
     "url": "/snscard/_nuxt/pages/account.0bfb4e628e1d1636c4fa.js",
     "revision": "a1deeace824b338e2f293090d6c50ac6"
   },
   {
-    "url": "/snscard/_nuxt/pages/index.5c111e6ef863616607d6.js",
-    "revision": "b4b4b2db43f113c28f0a4e99fbfde663"
+    "url": "/snscard/_nuxt/pages/index.4d91e69929c256b3776b.js",
+    "revision": "d3bd36e72c0b16b8b2f22dd7495af982"
   },
   {
     "url": "/snscard/_nuxt/vendor.e038a3388b5853c3db6b.js",
